ee44af1e051abbfdbe9b2fbbefc104de720e9ce0
n1k61n <eminelxanoglu@gmail.com>
2024-04-08 23:19:31 +0400

    add life

--- main.py
+++ main.py
@@ -1,25 +1,40 @@
 import os
+import time
 
 
 ROW = 10
 COL = 10
 
 world = [[" "] * COL for _ in range(ROW)]
-world[0][1] = "*"
-world[1][2] = "*"
-world[2][0] = "*"
-world[2][1] = "*"
-world[2][2] = "*"
 
+
+def init_life(arr, x, y):
+    arr[x + 0][y + 1] = "*"
+    arr[x + 1][y + 2] = "*"
+    arr[x + 2][y + 0] = "*"
+    arr[x + 2][y + 1] = "*"
+    arr[x + 2][y + 2] = "*"
+
+
+def clear_life(arr, x, y):
+    arr[x + 0][y + 1] = " "
+    arr[x + 1][y + 2] = " "
+    arr[x + 2][y + 0] = " "
+    arr[x + 2][y + 1] = " "
+    arr[x + 2][y + 2] = " "
 
 def print_world(arr):
-    os.system("cls")
     for i in range(ROW):
         print("".join(arr[i]))
 
 
 def main():
-    print_world(world)
+    for step in range(COL-2):
+        os.system("cls")
+        init_life(world, step, step)
+        clear_life(world, step-1,step-1)
+        print_world(world)
+        time.sleep(1)
 
 
 if __name__=="__main__":
